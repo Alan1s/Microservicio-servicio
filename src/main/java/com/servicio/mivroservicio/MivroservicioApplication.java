d06e281ddfd777cf0f350e84bc444aa863c94f6c
Luisa vargas <luchisvargasgomez@gmail.com>
2024-10-01 16:31:31 -0500

    creación de proyecto

---
+++ src/main/java/com/servicio/mivroservicio/MivroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.mivroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MivroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MivroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicio/mivroservicio/MivroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.mivroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MivroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
